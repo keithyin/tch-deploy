99ca06050247f9a72972336b6e16292ac2f3aaae
keithyin <yinpenghhz@hotmail.com>
2025-01-23 22:35:45 +0800

    m

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use tch::{self, Device, Kind, Tensor};
+
 fn main() {
-    println!("Hello, world!");
+    let pt_path = "/root/projects/tch-deploy/traced_model.pt";
+    let mut model = tch::CModule::load(pt_path).unwrap();
+    model.set_eval();
+
+    let inp = Tensor::rand([3, 10, 10], (Kind::Float, Device::Cpu));
+    let lengths = Tensor::from_slice(&[8, 9, 9]);
+    let res = model.forward_ts(&[inp, lengths]).unwrap();
+
+    res.print();
+    let mut res_vec: Vec<f32> = vec![0.0_f32; res.numel()];
+    res.copy_data(&mut res_vec, res.numel());
+    print!("{:?}", res_vec);
 }
