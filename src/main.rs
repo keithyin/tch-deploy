2d37617e5dec98cc4bfc0a5e4bf75150e63fe27e
keithyin <yinpenghhz@hotmail.com>
2025-01-23 22:39:34 +0800

    u

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,15 @@ fn main() {
 
     let inp = Tensor::rand([3, 10, 10], (Kind::Float, Device::Cpu));
     let lengths = Tensor::from_slice(&[8, 9, 9]);
-    let res = model.forward_ts(&[inp, lengths]).unwrap();
+
+    let res = {
+        let _guard = tch::no_grad_guard();
+        let res = model.forward_ts(&[inp, lengths]).unwrap();
+        res
+    };
 
     res.print();
     let mut res_vec: Vec<f32> = vec![0.0_f32; res.numel()];
     res.copy_data(&mut res_vec, res.numel());
-    print!("{:?}", res_vec);
+    println!("{:?}", res_vec);
 }
